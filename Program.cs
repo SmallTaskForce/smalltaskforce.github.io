24caafd9bd6e6268f09062aa971f07bd5d8d1bc2
SKIDDOW <achinsorry@gmail.com>
2026-01-08 20:39:52 +0530

    .

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
-using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MudBlazor.Services;
 using SmallTaskForceWeb;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,7 +7,4 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddMudServices();
-builder.Services.AddBlazoredLocalStorage();
-
 await builder.Build().RunAsync();
